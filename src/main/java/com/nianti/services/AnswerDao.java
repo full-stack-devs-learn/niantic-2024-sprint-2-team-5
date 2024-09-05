84423e45085f0b9a596582dd52154ace3bd56dab
Hannah Patzold <hannaholivia@myyahoo.com>
2024-09-05 15:11:08 -0700

    AnswerDao

--- src/main/java/com/nianti/services/AnswerDao.java
+++ src/main/java/com/nianti/services/AnswerDao.java
@@ -4,24 +4,43 @@ import com.nianti.models.Answer;
 import com.nianti.models.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class AnswerDao
-{
+public class AnswerDao {
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public AnswerDao(DataSource dataSource)
-    {
+    public AnswerDao(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public List<Answer> getAnswersByQuestionId(int questionId)
-    {
-        return null;
+    public List<Answer> getAnswersByQuestionId(int questionId) {
+        ArrayList<Answer> answers = new ArrayList<>();
+
+        String sql = """
+                SELECT answer_id
+                ,question_id
+                ,answer_text
+                ,is_correct
+                FROM answer
+                WHERE question_id = ?;
+                """;
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql, questionId);
+
+        while (row.next()) {
+            int answerId = row.getInt("answer-id");
+            String answerTest = row.getString("answer-text");
+            boolean isCorrect = row.getBoolean("is_correct");
+
+            Answer answer = new Answer(answerId, questionId, answerTest, isCorrect);
+            answers.add(answer);
+        }
+        return answers;
     }
 }
