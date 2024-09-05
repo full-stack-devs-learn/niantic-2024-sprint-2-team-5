3226681dca69f3a8d9aa238c84f76519ec2d6465
Dureti Shemsi <hamzadure@gmail.com>
2024-09-05 13:47:32 -0700

    added quiz controller and question DAO

--- src/main/java/com/nianti/controllers/QuizController.java
+++ src/main/java/com/nianti/controllers/QuizController.java
@@ -1,11 +1,12 @@
 package com.nianti.controllers;
 
-import ch.qos.logback.core.model.Model;
 import com.nianti.models.Quiz;
 import com.nianti.services.QuizDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -17,15 +18,14 @@ public class QuizController {
     @Autowired
     private QuizDao quizDao;
 
-    @GetMapping("/quiz/index")
-    public String getAllQuiz(Model model, @RequestParam(required = false) String quiz) {
-        ArrayList<Quiz> quizzes;
+    @GetMapping("quizzes/{quizId}")
+    public String getQuizById(Model model, @PathVariable int quizId)
+    {
+        var quiz = quizDao.getQuizById(quizId);
+        model.addAttribute("quiz", quiz);
+        model.addAttribute("pageTitle", "Get Quiz by Id");
 
-        quizzes = (ArrayList<Quiz>) quizDao.getAllQuizzes();
-
-      
-
-        return "/quiz/index";
+        return "quizzes/index";
     }
 }
 
--- src/main/java/com/nianti/services/QuestionDao.java
+++ src/main/java/com/nianti/services/QuestionDao.java
@@ -3,9 +3,11 @@ package com.nianti.services;
 import com.nianti.models.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -19,8 +21,28 @@ public class QuestionDao
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public List<Question> getQuestionByQuizId(int quizId)
-    {
-        return null;
+    public List<Question> getQuestionByQuizId(int quizId) {
+
+        ArrayList<Question> questions = new ArrayList<>();
+        String sql = """
+                SELECT question_id
+                         , quiz_id
+                         , question_number
+                         , question_text
+                FROM question
+                WHERE quiz_id =?;
+                """;
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql, quizId);
+
+        while (row.next()) {
+            int questionId = row.getInt("question_id");
+            int questionNumber = row.getInt("question_number");
+            String questionText = row.getString("question_text");
+
+            Question question= new Question(questionId, quizId,questionNumber, questionText);
+
+            questions.add(question);
+        }
+        return questions;
     }
 }
