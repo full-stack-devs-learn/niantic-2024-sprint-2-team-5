4fe13064eeca4b6b2574035bc814d295d9c98cce
Hannah Patzold <hannaholivia@myyahoo.com>
2024-09-07 00:55:44 -0700

    Next Button on page and removed Start button from Quiz

--- src/main/java/com/nianti/controllers/QuizController.java
+++ src/main/java/com/nianti/controllers/QuizController.java
@@ -23,5 +23,6 @@ public class QuizController {
 
         return "quiz/quiz-page";
     }
+
 }
 
--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -5,40 +5,54 @@ document.addEventListener("DOMContentLoaded", () => {
 
         let currentQuestionIndex = 0
         const startButton = document.getElementById("start-button")
-        document.getElementById("start-button").textContent = "Quiz started!"
-        const previousButton = document.getElementById("prev-button")
         const nextButton = document.getElementById("next-button")
-        
-    loadQuestion()
+        const quizContent = document.getElementById("quiz-content")
 
-    startButton.addEventListener('click', startQuiz);
-    nextButton.addEventListener('click, nextPage')
-});
+        document.getElementById("start-button").textContent = "Start Quiz!"
+
+
+        startButton.addEventListener('click', startQuiz);
+        nextButton.addEventListener('click', nextQuestion);
+
 
 function startQuiz(event)
 {
+startButton.style.display = 'none';
+nextButton.style.display = 'block';
 const quizId = document.getElementById("quiz-id").value;
 
     const url = `/questions/${quizId}`;
-     fetch(url).then(response => {
-            return response.text();
+     fetch(url)
+     .then(response => { return response.text();
         })
         .then(data => {
+
            document.getElementById('quiz-content').innerHTML = data;
-            //console.log(h3)
+
+
         })
         .catch(error => {
             console.log(error);
-        })
+        });
 }
 
-function loadQuestion()
-{
+function nextQuestion(event) {
+const quizId = document.getElementById("quiz-id").value
+currentQuestionIndex++;
+const url = `/questions/${quizId}/${currentQuestionIndex}`;
+
+fetch(url)
+.then(response => { return response.text();
+})
+.then(data => {
+
+document.getElementById('quiz-content').innerHTML = data;
+})
+.catch(error => {
+console.log(error);
+});
 }
 
-function nextPage(event) {
-    const quizId = document.getElementById("quiz-id").currentQuestionIndex++;
-}
-
+});
 
 
